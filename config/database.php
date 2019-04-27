20975f6dc093c1c67aaeef000208cff7fa13dbc3
Pavel Sklyar <ozpavel@yandex.ru>
2019-04-27 19:23:39 +0300

    Настройки для базы данных

--- config/database.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=ToDoListYii2',
-    'username' => 'homestead',
-    'password' => 'secret',
-    'charset' => 'utf8',
-];
